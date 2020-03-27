d6b3882c71b71bcaa5e712ecd603fe0b79d65839
Laila Campbell <lailacampbell96@gmail.com>
2020-03-27 15:20:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,4 +2,4 @@ require 'nokogiri'
 require 'open-uri'
 html = open("http://flatironschool.com/")
 doc = Nokogiri::HTML (html)
-print doc.css(".headline-26OIBN")  
+print doc.css(".headline-26OIBN").text   
